ac9928890b72ea15d115aa1578e187a680049f09
Harry <cra0zy@gmail.com>
2020-02-01 18:38:38 +0100

    Improve zsh completion script to allow filename selection (#4011)

--- scripts/register-completions.zsh
+++ scripts/register-completions.zsh
@@ -4,7 +4,15 @@ _dotnet_zsh_complete()
 {
   local completions=("$(dotnet complete "$words")")
 
-  reply=( "${(ps:\n:)completions}" )
+  # If the completion list is empty, just continue with filename selection
+  if [ -z "$completions" ]
+  then
+    _arguments '*::arguments: _normal'
+    return
+  fi
+
+  # This is not a variable assigment, don't remove spaces!
+  _values = "${(ps:\n:)completions}"
 }
 
-compctl -K _dotnet_zsh_complete dotnet
+compdef _dotnet_zsh_complete dotnet
