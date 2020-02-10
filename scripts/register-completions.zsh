16e626fcfcec257184c61d6d3f49d053df90f7e4
Shaun Tabone <shaun.tabone@gig.com>
2020-02-10 18:21:21 +0100

    Added dotnet new list of installed templates

--- scripts/register-completions.zsh
+++ scripts/register-completions.zsh
@@ -2,6 +2,16 @@
 
 _dotnet_zsh_complete() 
 {
+  # If command is dotnet new, display the list of installed templates
+  if [ $CURRENT -eq 3 ]; then
+    case ${words[2]} in
+      "new")
+      compadd -X ".NET Installed Templates" $(dotnet new -l | tail -n +21 | sed 's/  \+/:/g' | cut -d : -f 2)
+      return
+      ;;
+    esac
+  fi
+
   local completions=("$(dotnet complete "$words")")
 
   # If the completion list is empty, just continue with filename selection
